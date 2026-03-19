5993b4ea0f01537a4dca7ae4119e3f16128eff6e
tanonkim <robbins4bos@gmail.com>
2026-03-19 22:09:29 +0900

    1. 재고감소 동시 요청
    RaceCondition이 일어남
    
    (cherry picked from commit afaed653d5b77a6ebda68a4f29da32dab3229139)

---
+++ src/main/java/com/example/stock/service/StockService.java
@@ -0,0 +1,26 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockService {
+
+    private final StockRepository stockRepository;
+
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    public void decrease(Long id, Long quantity) {
+        // Stock 조회
+        // 재고를 감소
+        // 갱신된 값 저장
+
+        Stock stock = stockRepository.findById(id).orElseThrow();
+        stock.decrease(quantity);
+
+        stockRepository.saveAndFlush(stock);
+    }
+}
