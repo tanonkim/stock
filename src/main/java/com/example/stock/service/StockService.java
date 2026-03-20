f0d64b4486960ac688d311c95650f2fe42b5b89d
tanonkim <robbins4bos@gmail.com>
2026-03-21 08:41:19 +0900

    2. DB
    NamedLock
    분산 락을 구현할 때 주로 사용(세션 관리를 잘 해야 함)

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -14,8 +15,9 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-//    @Transactional
-    public synchronized void decrease(Long id, Long quantity) {
+    // 부모 스레드와 별도로 실행되어야 함
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void decrease(Long id, Long quantity) {
         // Stock 조회
         // 재고를 감소
         // 갱신된 값 저장
